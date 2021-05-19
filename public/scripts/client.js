aa9db4d827a76d4b9262e31a79c4abb32f1dde5d
JettScythe <jettscythe@hotmail.com>
2021-05-19 18:56:58 -0300

    delete tweetTime func, add functionality in client

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,28 +3,71 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
+const renderTweets = function(tweets) {
+// loops through tweets
+  for (const tweet of tweets) {
+    // calls createTweetElement for each tweet
+    const newTweet = createTweetElement(tweet);
+    // takes return value and appends it to the tweets container
+    $('#tweets-container').prepend(newTweet)
+  }
+}
+
+const escape = function (str) {
+  let div = document.createElement("div");
+  div.appendChild(document.createTextNode(str));
+  return div.innerHTML;
+};
+
+const createTweetElement = function(tweet) {
+  const safeText = escape(`${tweet.content.text}`)
+  const newTime = timeago.format(tweet.created_at);
+  let $tweet = 
+  `<article class="tweet" >
+  <header>
+    <!-- avatar & username -->
+    <div class="user">
+      <span><img src="${tweet.user.avatars}"></span>
+      <span>${tweet.user.name}</span>
+    </div>
+    <span class="handle">${tweet.user.handle}</span>
+  </header>
+  <!-- content of tweet -->
+  <h5>${safeText}</h5>
+  <footer>
+    <!-- time since posted & bookmark, retweet, like icons -->
+    <span class="time">${newTime}</span>
+    <div class="tweet-icons">
+        <i class="fas fa-flag"></i>
+        <i class="fas fa-retweet"></i>
+        <i class="fas fa-heart"></i>
+    </div> 
+  </footer>`
+return $tweet;
+}
+
+const loadTweets = function() {
+  $.get("/tweets", function(data) {
+    renderTweets(data);
+  });
+}
+
+
+
 $(document).ready(function() {
-  // Test / driver code (temporary). Eventually will get this from the server.
-  const tweetData = {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png",
-        "handle": "@SirIsaac"
-      },
-    "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-    "created_at": 1461116232227
-  }
-
-  const createTweetElement = (db) => {
-    
-  }
-
-  const $tweet = $(`<article class="tweet">Hello World</article>`);
-  //const $tweet = createTweetElement(tweetData);
-
-  // Test / driver code (temporary)
-  console.log($tweet); // to see what it looks like
-  $('#tweets-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
+  loadTweets();
+    $("#new-tweet-form").submit(function(event) {
+      event.preventDefault();
+      if ($("#tweet-text").val().length <= 0) {
+        alert("Ya gotta type somethin'!")
+      } else if ($("#tweet-text").val().length >= 140) {
+        alert("Too many characters. Please shrink to my arbitrary limits lmao")
+      } else {
+        $.post("/tweets", $(this).serialize()).then(function( data ) {
+            $("#tweet-text").val("")
+            $(".counter").val("140")
+            loadTweets();
+        });
+      }
+    });
 });
--- public/scripts/tweet-time.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function() {
-  const newTime = timeago.format(new Date());
-  const timeClass = $(".time");
-  timeClass.text(newTime);
-})
